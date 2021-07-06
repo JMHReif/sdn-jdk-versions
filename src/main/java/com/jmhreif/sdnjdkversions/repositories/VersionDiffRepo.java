80b3713e145139a29b7ba13dd7234c4be079feb6
Jennifer Reif <jennifer.reif@neo4j.com>
2021-07-06 14:36:00 -0500

    Modify method queries and add local connection properties

--- src/main/java/com/jmhreif/sdnjdkversions/repositories/VersionDiffRepo.java
+++ src/main/java/com/jmhreif/sdnjdkversions/repositories/VersionDiffRepo.java
@@ -6,10 +6,10 @@ import org.springframework.data.neo4j.repository.query.Query;
 
 public interface VersionDiffRepo extends Neo4jRepository<VersionDiff, Long> {
 
-    @Query("MATCH (:JavaVersion {version: $from})-[:FROM_NEWER]->(v:VersionDiff)-[r1:HAS_DELTA]->(d1)-[r2:HAS_DELTA]->(d2) RETURN v, collect(r1), collect(d1), collect(r2), collect(d2);")
+    @Query("MATCH (:JavaVersion {version: $from})-[:NEWER]->(v:VersionDiff)-[r1:HAS_DELTA]->(d1) RETURN v, collect(r1), collect(d1);")
     Iterable<VersionDiff> findVersionDiffsBy(String from);
 
-    @Query("MATCH (:JavaVersion {version: $from})<-[:TO_OLDER]-(v:VersionDiff)<-[:FROM_NEWER]-(:JavaVersion {version: $to})" +
-            "MATCH (v)-[r1:HAS_DELTA]->(d1)-[r2:HAS_DELTA]->(d2) RETURN v, collect(r1), collect(d1), collect(r2), collect(d2);")
+    @Query("MATCH (:JavaVersion {version: $from})-[:NEWER]->(v:VersionDiff)<-[:OLDER]-(:JavaVersion {version: $to})" +
+            "MATCH (v)-[r1:HAS_DELTA]->(d1) RETURN v, collect(r1), collect(d1);")
     VersionDiff findVersionDiffBetween(String from, String to);
 }
